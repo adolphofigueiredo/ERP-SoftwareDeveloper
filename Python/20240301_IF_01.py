3780fc639cea64edbcc932cf224123533d88c073
adolphofigueiredo <adolphoneto@gmail.com>
2024-06-11 15:48:28 +0200

    Update 20240301_IF_01.py
    Using only IF

--- Python/20240301_IF_01.py
+++ Python/20240301_IF_01.py
@@ -6,10 +6,13 @@ Using these quotation marks you can make comments before starting the program.
 
 a = int(input("Enter the first number: "))
 b = int(input("Enter the second number: "))
-if a>0 and b>0:
-    print("Both are positives!")
+if (a>0):
+    if (b>0):
+        print("Both are positives!")
+    else:
+        print("There is one positive number and a negative number!")
 else:
-    if a<0 and b<0:
+    if (b<0):
         print("Both are negatives!")
     else:
         print("There is one positive number and a negative number!")

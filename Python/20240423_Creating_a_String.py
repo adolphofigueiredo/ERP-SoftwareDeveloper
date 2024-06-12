acdda4e2341e86d44ca80546571f6248fdb91fa6
adolphofigueiredo <adolphoneto@gmail.com>
2024-06-12 13:23:56 +0200

    Using STRING

---
+++ Python/20240423_Creating_a_String.py
@@ -0,0 +1,12 @@
+str1 = "First string"
+print(str1)
+str2 = 'Second string'
+print(str2)
+str3 = """Third string"""
+print(str3)
+str4 = "'Fourth' string"
+print(str4)
+str5 = '"Fiveth" string'
+print(str5)
+str6 = "Sixth\" string"
+print(str6)

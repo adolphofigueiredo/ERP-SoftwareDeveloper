abcb1e90b7ae330925f8a342048c9762fe951ab9
adolphofigueiredo <adolphoneto@gmail.com>
2024-06-12 10:02:19 +0200

    Using IF

---
+++ Python/20240301_Driving_License_01.py
@@ -0,0 +1,15 @@
+'''
+In Europe, you can get a Category B driving license if you are at least 18 years old.
+Design a program that, given the input age, indicates whether the user can obtain
+the license.
+'''
+
+age = int(input("Enter your age: "))
+
+if age >= 18:
+
+    print("You can get your driver's license!")
+
+else:
+
+    print("You can't get your driver's license")

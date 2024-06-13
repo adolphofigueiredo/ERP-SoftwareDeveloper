b756545a51d334ec822d535bf40b1b5e054dd59e
adolphofigueiredo <adolphoneto@gmail.com>
2024-06-13 22:46:26 +0200

    Using FOR and ENUMERATE

---
+++ Python/20240613_Enumerate_01.py
@@ -0,0 +1,9 @@
+Fruit = ['Apple', 'Banana', 'Kiwi']
+
+for index, value in enumerate(Fruit, start=1):
+    print(f"Fruit {index}: {value}")
+
+# Print:
+# Fruit 1: Apple
+# Fruit 2: Banana
+# Fruit 3: Kiwi

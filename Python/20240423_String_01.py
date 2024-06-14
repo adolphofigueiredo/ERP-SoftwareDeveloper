3c3808173efe21606f3380878b76c90a2f31f3e1
adolphofigueiredo <adolphoneto@gmail.com>
2024-06-14 09:15:11 +0200

    Using STRING

---
+++ Python/20240423_String_01.py
@@ -0,0 +1,6 @@
+day = input('What is the day of your birth? ')
+month = input('What is the month of your birth? ')
+year = input('What is the year of your birth? ')
+
+print('You were born on', day, 'of', month, 'of', year, ', correct?')
+

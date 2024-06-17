4800a7179ec31151b684307b1cb62e78d1562320
adolphofigueiredo <adolphoneto@gmail.com>
2024-06-17 14:08:03 +0200

    Using DEF

---
+++ Python/20240415_Def_01.py
@@ -0,0 +1,5 @@
+def areaRettangolo(base, altezza, profondita):
+    return base*altezza*profondita
+
+print("Area 1 = ", areaRettangolo(3,profondita=4,altezza=5))
+print("Area 2 = ", areaRettangolo(profondita=4,altezza=5,base=3))

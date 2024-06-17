0de277dd4a9a2011c5f07d6bd30fed8cd420a6c2
adolphofigueiredo <adolphoneto@gmail.com>
2024-06-17 14:06:04 +0200

    Using RANDOM and FOR

---
+++ Python/20240319_Random_01.py
@@ -0,0 +1,41 @@
+accum = 0
+print("\n")
+
+from random import *
+for i in range(1, 10):
+    print(seed(9), end=" | ")
+
+print("\n")
+
+import random as r
+for i in range(1, 6):
+    num = -r.random()
+    print(num, end=" | ")
+    accum = accum + num
+    print(accum) 
+
+print("\n")
+
+from random import *
+for i in range(1, 6):
+    print(random(), end=" | ")
+
+print("\n")
+
+from random import *
+for i in range(1, 30):
+    print(randrange(5), end=" | ")
+
+print("\n")
+
+from random import *
+for i in range(1, 30):
+    print(randrange(1, 8), end=" | ")
+
+print("\n")
+
+from random import *
+for i in range(1, 30):
+    print(randrange(1, 8, 2), end=" | ")
+
+print("\n")

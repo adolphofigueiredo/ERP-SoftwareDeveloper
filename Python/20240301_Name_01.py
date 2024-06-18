cbf18ac3939358b8a2d2b9040d339ae2b8e6ba40
adolphofigueiredo <adolphoneto@gmail.com>
2024-06-18 13:52:35 +0200

    Using IF and ELSE

---
+++ Python/20240301_Name_01.py
@@ -0,0 +1,6 @@
+name = input('What is your name? ')
+if name == 'Adolpho':
+    print('What a beautiful name you have!')
+else:
+    print('Your name is so normal!')
+print('Good morning, {}!'.format(name))

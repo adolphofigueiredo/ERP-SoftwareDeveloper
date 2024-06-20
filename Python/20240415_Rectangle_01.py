129aa193d281a547611b497f06ac29ad6bfa164b
adolphofigueiredo <adolphoneto@gmail.com>
2024-06-20 11:22:14 +0200

    Using DEF

---
+++ Python/20240415_Rectangle_01.py
@@ -0,0 +1,6 @@
+def rectangleArea(base = 2, height = 4):
+    return base * height
+
+print("Area 1 = ", rectangleArea())
+print("Area 2 = ", rectangleArea(5))
+print("Area 3 = ", rectangleArea(5, 5))

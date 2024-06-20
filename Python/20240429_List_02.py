d69f7ee64bf19c8902ce5ad0769271257aca3687
adolphofigueiredo <adolphoneto@gmail.com>
2024-06-20 12:00:13 +0200

    Using LIST and FOR

---
+++ Python/20240429_List_02.py
@@ -0,0 +1,17 @@
+lista = [-10, "hello", 34.6, True]
+
+print(lista[0], lista[1], lista[3])
+print(len(lista))
+print(lista[-4], lista[-1])
+
+i = 1
+
+print(lista[i + 1])
+
+for i in range(len(lista)):
+    print(i, ": ", lista[i])
+
+print(lista)
+
+for element in lista:
+    print(element)

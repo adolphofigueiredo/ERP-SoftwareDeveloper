754201303d362f69b51a5866bf73595e8458323d
Adolpho.Figueiredo <adolphoneto@gmail.com>
2024-06-20 20:49:53 +0200

    Using IF and ELSE

---
+++ Python/20240301_If_and_Else_01.py
@@ -0,0 +1,21 @@
+# This is a comment
+
+'''
+
+I can write multiple
+lines of comment
+
+'''
+
+a = int(input("A: "))
+b = int(input("B: "))
+if (a > 0):
+    if (b > 0):
+        print("POSITIVE")
+    else:
+        print("DISCORDANT")
+else:
+    if (b < 0):
+        print("NEGATIVE")
+    else:
+        print("DISCORDANT")

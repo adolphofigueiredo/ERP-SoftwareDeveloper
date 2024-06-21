3b7654960ac04257280a988d78427426d329ef1c
Adolpho.Figueiredo <adolphoneto@gmail.com>
2024-06-21 13:44:41 +0200

    Using STR

---
+++ Python/20240301_Born_Date_01.py
@@ -0,0 +1,4 @@
+day = input('What day were you born on? ')
+month = input('What month were you born in? ')
+year = input('What year were you born in? ')
+print('You were born on', day, 'of', month, 'of', year, ', is that correct?')

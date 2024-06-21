327c6d8c3c43b938e47a9c44fcd4be6874d1547e
Adolpho.Figueiredo <adolphoneto@gmail.com>
2024-06-21 13:46:09 +0200

    Correction

--- Python/AAAAAAAAAA/Aula_004_002_Data de nascimento.py
+++
@@ -1,4 +0,0 @@
-dia = input ('Qual o dia do se nascimento?')
-mês = input ('Qual o mês do se nascimento?')
-ano = input ('Qual o ano do se nascimento?')
-print ('Você nasceu no dia',dia,'de',mês,'de',ano,',correto?')

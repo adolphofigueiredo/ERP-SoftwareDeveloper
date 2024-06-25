1547afba62e496e8fbaec9ffd6b6a10c382ed5e6
Adolpho.Figueiredo <adolphoneto@gmail.com>
2024-06-25 10:54:26 +0200

    Correction

--- Python/AAAAAAAAAA/Aula_015_001_Interrompendo_Repeticoes_While.py
+++
@@ -1,8 +0,0 @@
-n = s = 0
-while True:
-    n = int(input('Digite um número: '))
-    if n == 999:
-        break
-    s += n
-print('A soma de todos os números é {}.'.format(s))
-print(f'A soma de todos os números é {s}.')

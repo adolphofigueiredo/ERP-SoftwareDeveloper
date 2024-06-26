eb0feed3b3b7fcc891f3e90daa0e539580f34ad8
Adolpho.Figueiredo <adolphoneto@gmail.com>
2024-06-26 13:39:57 +0200

    Correction

--- Python/AAAAAAAAAA/Aula_008_001_Importar_Generico.py
+++
@@ -1,7 +0,0 @@
-import math
-num = int(input('Digite um número: '))
-raiz = math.sqrt(num)
-print('A raiz de {} é igual a {}.'.format(num,raiz))
-print('A raiz de {} é igual a {:.2f}.'.format(num,raiz))
-print('A raiz de {} é igual a {}.'.format(num,math.ceil(raiz)))
-print('A raiz de {} é igual a {}.'.format(num,math.floor(raiz)))

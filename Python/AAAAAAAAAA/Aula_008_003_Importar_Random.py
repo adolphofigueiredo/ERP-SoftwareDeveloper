76a2c7be0b4589ede856e1f39ce1add98abcff59
Adolpho.Figueiredo <adolphoneto@gmail.com>
2024-06-26 14:00:53 +0200

    Correction

--- Python/AAAAAAAAAA/Aula_008_003_Importar_Random.py
+++
@@ -1,4 +0,0 @@
-import random
-num = random.random
-num2 = random.randint(1,10)
-print('Resultado',num,num2,)
--- Python/AAAAAAAAAA/Aula_008_004_Importar_Emoji.py
+++
@@ -1,3 +0,0 @@
-import emoji
-print(emoji.emojize('Olá, Mundo :sunglasses:',use_aliases=True))
-print(emoji.emojize('Olá, Mundo :earth_americas:',use_aliases=True))

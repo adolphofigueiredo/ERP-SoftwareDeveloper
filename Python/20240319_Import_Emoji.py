570d797dda90f3cad06036f8a7ff9823a5849330
Adolpho.Figueiredo <adolphoneto@gmail.com>
2024-06-26 14:08:34 +0200

    Using IMPORT

---
+++ Python/20240319_Import_Emoji.py
@@ -0,0 +1,14 @@
+'''
+Before importing the emoji library, you need to install it through the command line (cmd).
+It is necessary to access it and type: "pip install emoji"
+https://www.webfx.com/tools/emoji-cheat-sheet/
+https://github.com/carpedm20/emoji?tab=readme-ov-file
+'''
+
+# Seção de importação da biblioteca
+import emoji  # Importa a biblioteca emoji
+
+# Seção de saída
+print(emoji.emojize('Olá, Mundo :sunglasses:'))  # Imprime "Olá, Mundo" com emoji de óculos de sol
+print(emoji.emojize('Olá, Mundo :red_heart:'))  # Imprime "Olá, Mundo" com emoji do globo terrestre
+print(emoji.emojize('Olá, Mundo :dashing_away:'))

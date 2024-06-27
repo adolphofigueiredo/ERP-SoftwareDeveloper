39216de7e03f7c861c44b91d2842c55eb6a0579e
Adolpho.Figueiredo <adolphoneto@gmail.com>
2024-06-27 13:37:46 +0200

    Using STR and SLICING

---
+++ Python/20240423_String_07_Slice.py
@@ -0,0 +1,13 @@
+sentence = 'Adolpho Silverio Figueiredo Neto'
+print('sentence[0]    ', sentence[0])
+print('sentence[:7]   ', sentence[:7])
+print('sentence[7:]   ', sentence[7:])
+print('sentence[8:]   ', sentence[8:])
+print('sentence[0:7]  ', sentence[0:7])
+print('sentence[0:7:2]', sentence[0:7:2])
+print('sentence[0::2] ', sentence[0::2])
+print('sentence[::-1] ', sentence[::-1])
+print("""The government pays this Monday (20th) the first installment of
+ the emergency aid of R$ 600 for 4.2 million people who
+ registered through the Caixa app or website and will receive it
+ in Caixa digital savings accounts automatically opened upon registration.""")

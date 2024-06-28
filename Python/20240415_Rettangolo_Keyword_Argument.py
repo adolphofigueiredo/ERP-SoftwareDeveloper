c238e43ddfbb9662f15db622da225307bf37718d
Adolpho.Figueiredo <adolphoneto@gmail.com>
2024-06-28 09:25:40 +0200

    Using DEF and KEYWORD ARGUMENT

---
+++ Python/20240415_Rettangolo_Keyword_Argument.py
@@ -0,0 +1,5 @@
+def volumeRettangolo(base, altezza, profondita):
+    return base * altezza * profondita
+
+print("Volume 1 = ", volumeRettangolo(3, profondita=4, altezza=5))
+print("Volume 2 = ", volumeRettangolo(profondita=4, altezza=5, base=3))

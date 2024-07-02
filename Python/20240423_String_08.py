5b115d8a7a1dce309375c16e6bf6843e231b3e31
Adolpho.Figueiredo <adolphoneto@gmail.com>
2024-07-02 08:30:36 +0200

    Using STRING

---
+++ Python/20240423_String_08.py
@@ -0,0 +1,7 @@
+str = "Stringa"
+substring = str[2:4]
+print(substring)       # Output: 'ri'
+print(str[0:1])        # Output: 'S'
+print(str[:4])         # Output: 'Stri'
+print(str[5:])         # Output: 'ga'
+print(str[-3:-1])      # Output: 'ng'

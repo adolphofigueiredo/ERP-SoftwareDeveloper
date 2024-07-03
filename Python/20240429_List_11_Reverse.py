1e4f0485b49de1803d8bb5b69ebe107425f78aaf
Adolpho.Figueiredo <adolphoneto@gmail.com>
2024-07-03 21:44:10 +0200

    Using LIST FOR and REVERSE

--- Python/20240429_List_11_Reverse.py
+++ Python/20240429_List_11_Reverse.py
@@ -1,35 +1,35 @@
 '''
-Autore: Adolpho Silverio Figueiredo Neto
-Data: 07/05/2024
+Author: Adolpho Silverio Figueiredo Neto
+Date: 07/05/2024
 
-Esercizio 01
+Exercise 01
 
-Memorizzare in una lista di 100 posti i primi cento numeri naturali in ordine inverso.
+Store in a list of 100 positions the first one hundred natural numbers in reverse order.
 '''
 print("----------1-----------")
 Lista = list()
-for x in range(1,101):
+for x in range(1, 101):
     Lista += [x]
 Lista.reverse()
 
 print(Lista)
 print("----------2-----------")
 Lista = list()
-for x in range(0,100):
+for x in range(0, 100):
     Lista += [x]
 Lista.reverse()
 
 print(Lista)
 print("----------3-----------")
 Lista = list()
-for x in range(0,100):
-    Lista += [100-x]
+for x in range(0, 100):
+    Lista += [100 - x]
 
 print(Lista)
 print("----------4-----------")
 Lista = list()
-for x in range(100,0,-1):
+for x in range(100, 0, -1):
     Lista += [x]
 
 print(Lista)
-print("----------------------")    
+print("----------------------")

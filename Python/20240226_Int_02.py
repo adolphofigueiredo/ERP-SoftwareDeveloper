4926254465ff96cd14a887f1161bb6c2798cd5f3
Adolpho.Figueiredo <adolphoneto@gmail.com>
2024-07-03 20:11:05 +0200

    Using SUM and OPERATORS

---
+++ Python/20240226_Int_02.py
@@ -0,0 +1,27 @@
+# Operators
+
+# Addition + -> 5 + 2 = 7
+# Subtraction - -> 5 - 2 = 3
+# Multiplication * -> 5 * 2 = 10
+# Division / -> 5 / 2 = 2.5
+# Power ** -> 5 ** 2 = 25
+# Floor Division // -> 5 // 2 = 2
+# Modulus % -> 5 % 2 = 1
+
+print(5 + 2)  # Addition
+print(5 - 2)  # Subtraction
+print(5 * 2)  # Multiplication
+print(5 / 2)  # Division
+print(5 ** 2) # Power
+print(5 // 2) # Floor Division
+print(5 % 2)  # Modulus
+
+# Order of Operations
+
+# 1 -> ()
+# 2 -> **
+# 3 -> * / // %
+# 4 -> + -
+
+print(5 ** 2)      # Power using ** operator
+print(pow(5, 2))   # Power using pow() function
--- Python/AAAA/Aula_007_004_Potencia.py
+++
@@ -1,2 +0,0 @@
-print(5 ** 2)
-print(pow(5,2))

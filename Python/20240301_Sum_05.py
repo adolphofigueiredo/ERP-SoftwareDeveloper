dc9f080b335f4802b3f96122d7ef8ffefb9c2473
Adolpho.Figueiredo <adolphoneto@gmail.com>
2024-07-03 21:20:52 +0200

    Using SUM

---
+++ Python/20240301_Sum_05.py
@@ -0,0 +1,4 @@
+n1 = float(input('Enter a value = '))
+n2 = float(input('Enter another value = '))
+s = n1 + n2
+print('The sum of the value {} and the value {} is {}.'.format(n1, n2, s))
--- Python/AAAA/Aula_006_Exercicio_003_Soma.py
+++
@@ -1,5 +0,0 @@
-n1 = float(input('Digite um valor = '))
-n2 = float(input('Digite outro valor = '))
-s = n1 + n2
-print('A soma do valor {} e o valor {} é {}.'.format(n1, n2, s))
-

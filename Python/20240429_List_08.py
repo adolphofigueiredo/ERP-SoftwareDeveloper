0fde95e56c95e52bf4a5f5627ab5f4b434b03d43
Adolpho.Figueiredo <adolphoneto@gmail.com>
2024-07-03 21:19:24 +0200

    Using LIST

---
+++ Python/20240429_List_08.py
@@ -0,0 +1,13 @@
+# List with various elements
+lista = [-10, "hello", 34.6, True, 5, 6, 7]
+
+# Subsequence of the list
+subsequence = lista[2:4]
+print(subsequence)  # Prints elements at index 2 and 3
+
+# Printing different slices of the list
+print(lista[0:1])   # Prints the element at index 0
+print(lista[:4])    # Prints elements from the beginning to index 3
+print(lista[5:])    # Prints elements from index 5 to the end
+print(lista[-3:-1]) # Prints elements from the third to the last but one
+print(lista[0:7])   # Prints the whole list

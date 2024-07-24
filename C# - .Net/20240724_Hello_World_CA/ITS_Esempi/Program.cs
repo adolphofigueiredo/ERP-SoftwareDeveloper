c47ebb57e0fdd78986fd491b0208da1d8c7fd4bb
Adolpho Figueiredo <adolphoneto@gmail.com>
2024-07-24 20:23:39 +0200

    20240724_Lezione

---
+++ C# - .Net/20240724_Hello_World_CA/ITS_Esempi/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ITS_Esempi
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!!!");                        //Comando usado para escrever alguma coisa na tela.
+            Console.WriteLine("What's your name?");
+            string valore01 = Console.ReadLine();                       //Leitura da resposta e armazenamento na variavel.
+            Console.WriteLine("Nice to meet you " + valore01 + ".");
+            Console.ReadLine();                                         //Foi adicionado para o console nao fechar.
+                                                                        //Se nao for adicionado o console fecha sem ter tempo de ler.
+
+                                                                        //Fazer o input de dois numero e imprimir a soma.
+        }
+    }
+}

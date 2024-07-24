a29d2dea95a484ff09cd6e37ca79783938895030
Adolpho Figueiredo <adolphoneto@gmail.com>
2024-07-24 21:48:07 +0200

    20240724_Lezione

--- C# - .Net/20240724_Hello_World_CA/ITS_Esempi/Program.cs
+++ C# - .Net/20240724_Hello_World_CA/ITS_Esempi/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.Remoting.Services;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -12,12 +13,21 @@ namespace ITS_Esempi
         {
             Console.WriteLine("Hello World!!!");                        //Comando usado para escrever alguma coisa na tela.
             Console.WriteLine("What's your name?");
-            string valore01 = Console.ReadLine();                       //Leitura da resposta e armazenamento na variavel.
-            Console.WriteLine("Nice to meet you " + valore01 + ".");
+            string value01 = Console.ReadLine();                        //Leitura da resposta e armazenamento na variavel.
+            Console.WriteLine("Nice to meet you " + value01 + ".");
             Console.ReadLine();                                         //Foi adicionado para o console nao fechar.
                                                                         //Se nao for adicionado o console fecha sem ter tempo de ler.
+            Console.WriteLine("Enter the first number:");
+            int value02 = int.Parse(Console.ReadLine());                //Fazer o input de um numero.
 
-                                                                        //Fazer o input de dois numero e imprimir a soma.
+            Console.WriteLine("Enter the second number:");
+            int value03 = int.Parse(Console.ReadLine());                //Fazer o input do segunmdo numero.
+
+            int value04 = value02 + value03;                            //Criacao da variavel para fazer a soma.
+
+            Console.WriteLine("The sum of the numbers " + value02 + " and " + value03 + " is " + value04 + ".");
+
+            Console.ReadLine();                                         //Foi adicionado para o console nao fechar.
         }
     }
 }

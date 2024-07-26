3e3ccec6bd864771e1819b4f3237017918bd72f2
Adolpho Figueiredo <adolphoneto@gmail.com>
2024-07-26 17:57:04 +0200

    20240726_Lezione

--- C# - .Net/20240726_Calculator/20240726_Calculator/Program.cs
+++ C# - .Net/20240726_Calculator/20240726_Calculator/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,7 +9,6 @@ namespace _20240726_Calculator
 {
     internal class Program
     {
-
         static int RichiediValoreNumerico(string messaggio)
         {
             int ivalue02 = 0;
@@ -20,23 +20,20 @@ namespace _20240726_Calculator
                 Console.WriteLine(messaggio);
                 value02 = Console.ReadLine();                                                   //Fazer o input de um numero.
                 isInt = int.TryParse(value02, out ivalue02);
-                if (!isInt) Console.WriteLine("It is not a number");                            //O ponto de exclamaÇao significa not.
+                if (!isInt) Console.WriteLine("\r\nIt is not a number");                        //O ponto de exclamaÇao significa not.
             }
             while (!isInt);
             return ivalue02;
         }
-
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello!!!");                                                      //Comando usado para escrever alguma coisa na tela.
+            Console.WriteLine("\r\n          CALCULATOR\r\n");                                          //Comando usado para escrever alguma coisa na tela.
 
-            int value02 = RichiediValoreNumerico("Enter the first number:");
+            int value02 = RichiediValoreNumerico("\r\nEnter the first number:");
 
-
-            int value03 = RichiediValoreNumerico("Enter the second number:");
-
+            int value03 = RichiediValoreNumerico("\r\nEnter the second number:");
             
-            Console.WriteLine("Select operator: ");
+            Console.WriteLine("\r\nSelect operator: ");
             string operation = Console.ReadLine();
 
             int value04 = 0;
@@ -60,13 +57,19 @@ namespace _20240726_Calculator
                     break;
 
                 default:
-                    Console.WriteLine("Errore");
+                    Console.WriteLine("\r\nErrore");
                     break;
             }
 
-            Console.WriteLine("The result is " + value04 + ".");
+            Console.WriteLine("\r\nThe result is " + value04 + ".");
 
             Console.ReadLine();                                         //Foi adicionado para o console nao fechar.
         }
     }
 }
+
+
+
+// fazer uma calculadora que faca varias operacoes
+// usar a multiplicacao fazendo varias somas 
+// fazer a divisao usando a subtracao

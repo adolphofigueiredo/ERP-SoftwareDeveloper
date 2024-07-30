352995651370b6a981088f7b43097d9b5f9422ae
Adolpho Figueiredo <adolphoneto@gmail.com>
2024-07-30 22:41:47 +0200

    20240730_Lezione

---
+++ C# - .Net/20240725_Hello_World_II/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld_CA
+{
+    internal class Program
+    {
+        static int RichiediValoreNumerico(string messaggio)
+        {
+            int iAddendo1 = 0;
+            string addendo1 = "";
+            bool isInt = false;
+            do
+            {
+                Console.WriteLine(messaggio);
+                addendo1 = Console.ReadLine();
+                isInt = int.TryParse(addendo1, out iAddendo1);
+                if (!isInt) Console.WriteLine("Input non corretto!");
+            }
+            while (!isInt);
+            return iAddendo1;
+        }
+        static int RichiediIntero(string messaggio)
+        {
+            bool ilNumeroInseritoEIntero = false;
+            int iNumeroInserito = 0;
+            do
+            {
+                Console.WriteLine(messaggio);
+                string sNumeroInserito = Console.ReadLine();
+                ilNumeroInseritoEIntero = int.TryParse(sNumeroInserito, out iNumeroInserito);
+                if (ilNumeroInseritoEIntero == false)
+                {
+                    Console.WriteLine("input non corretto");
+                }
+
+            } while (ilNumeroInseritoEIntero == false);
+
+            return iNumeroInserito;
+        }
+
+        static int Main(string[] args)
+        {
+            Console.WriteLine("---- sommiamo 2 numeri ----");
+
+            int a1 = RichiediIntero("inserisci primo numero:");
+            int a2 = RichiediIntero("inserisci secondo numero:");
+
+            int risultato = a1 + a2;
+
+            Console.WriteLine("il risultato è:" + risultato);
+
+            if (risultato % 2 == 0)
+            {
+                Console.WriteLine("il risultato è: PARI");
+            }
+            else
+            {
+                Console.WriteLine("il risultato è: DISPARI");
+            }
+
+            // altro modo:
+            //Console.WriteLine("il risultato è:" + (risultato % 2 == 0 ? "PARI" : "DISPARI"));
+
+            Console.ReadLine();
+
+            return 0;
+
+
+        }
+
+    }
+}
---
+++ C# - .Net/20240726_Calculator_V1.0/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calcolatrice_CA
+{
+    internal class Program
+    {
+        static string RichiediOperazione()
+        {
+            string operazione = "";
+            bool operazioneAbilitata = false;
+            do
+            {
+                Console.WriteLine("Inserisci l'operazione che vuoi effettuare:[+,-,*,/]");
+                operazione = Console.ReadLine();
+                List<string> operazioniDisponibili = new List<string>(){
+                    "+","-","*","/"
+                };
+                operazioneAbilitata = operazioniDisponibili.Contains(operazione);
+                if (!operazioneAbilitata) Console.WriteLine("operazione non disponibile!");
+            }
+            while (!operazioneAbilitata);
+            return operazione;
+        }
+
+        static int RichiediValoreNumerico(string messaggio)
+        {
+            int iNumero = 0;
+            string sNumero = "";
+            bool eUnNumeroIntero = false;
+            do
+            {
+                Console.WriteLine(messaggio);
+                sNumero = Console.ReadLine();
+                eUnNumeroIntero = int.TryParse(sNumero, out iNumero);
+                if (!eUnNumeroIntero) Console.WriteLine("Input non corretto!");
+            }
+            while (!eUnNumeroIntero);
+            return iNumero;
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("CALCOLATRICE:");
+            string operazione = RichiediOperazione();
+            int n1 = RichiediValoreNumerico("Inserisci il primo numero");
+            int n2 = RichiediValoreNumerico("Inserisci il secondo numero");
+            int r = 0;
+            switch (operazione)
+            {
+                case "+": r = n1 + n2; break;
+                case "-": r = n1 - n2; break;
+                case "*": r = n1 * n2; break;
+                case "/": r = n1 / n2; break;
+            }
+
+            Console.WriteLine("risultato: " + r + " - è un numero " + (r % 2 == 0 ? "PARI" : "DISPARI"));
+            Console.ReadLine();
+
+        }
+    }
+}

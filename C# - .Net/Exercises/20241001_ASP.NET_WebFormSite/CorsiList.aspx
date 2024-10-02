bdfee0a329006e88bc2f8e3210a39771a530216c
Adolpho Figueiredo <adolphoneto@gmail.com>
2024-10-03 00:01:11 +0200

    20241002_UPDATE_HOMEWORK

---
+++ C# - .Net/Exercises/20241001_ASP.NET_WebFormSite/CorsiList.aspx
@@ -0,0 +1,28 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site.master" AutoEventWireup="true" CodeFile="CorsiList.aspx.cs" Inherits="CorsiList" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="MainContent" Runat="Server">
+    <div>
+        
+        <asp:Label ID="lblNominativoCorso" AssociatedControlID="txtNominativoCorso" runat="server" Text="Corso"></asp:Label>
+        <asp:TextBox ID="txtNominativoCorso" runat="server"></asp:TextBox>
+        <asp:Label ID="lblInizioCorsoDa" AssociatedControlID="calInizioCorsoDa" runat="server" Text="Data di inizio di corso da: "></asp:Label>
+        <asp:Calendar ID="calInizioCorsoDa" runat="server"></asp:Calendar>
+        <asp:Label ID="lblInizioCorsoA" AssociatedControlID="calInizioCorsoA" runat="server" Text="Data di inizio di corso a: "></asp:Label>
+        <asp:Calendar ID="calInizioCorsoA" runat="server"></asp:Calendar>
+        <asp:Label ID="lblFineCorsoDa" AssociatedControlID="calFineCorsoDa" runat="server" Text="Data di fine di corso da: "></asp:Label>
+        <asp:Calendar ID="calFineCorsoDa" runat="server"></asp:Calendar>
+        <asp:Label ID="lblFineCorsoA" AssociatedControlID="calFineCorsoA" runat="server" Text="Data di fine di corso a: "></asp:Label>
+        <asp:Calendar ID="calFineCorsoA" runat="server"></asp:Calendar>
+        
+        <asp:Button ID="btnRicerca" runat="server" Text="Search" OnClick ="btnRicerca_Click" />
+
+    </div>
+
+    <div>
+
+        <asp:GridView ID="grdRisultati" runat="server"></asp:GridView>
+
+    </div>
+
+</asp:Content>
+
---
+++ C# - .Net/Exercises/20241001_ASP.NET_WebFormSite/CorsiList.aspx.cs
@@ -0,0 +1,73 @@
+﻿using _20241001_BusinessLayer.Models.Dtos;
+using _20241001_BusinessLayer.Models.Entities;
+using _20241001_BusinessLayer.Models.Filter;
+using _20241001_BusinessLayer.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using _20241001_BusinessLayer.Models.Mappers;
+
+
+
+public partial class CorsiList : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }
+    protected void btnRicerca_Click(object sender, EventArgs e)
+    {
+
+        CorsoRepository repository = new CorsoRepository();
+        var filtro = new CorsoFilter
+        {
+
+
+            NominativoCorso = txtNominativoCorso.Text,
+            InizioCorsoDa = null,
+            InizioCorsoA = null,
+            FineCorsoDa = null,
+            FineCorsoA = null
+        };
+
+        if (calInizioCorsoDa.SelectedDate != DateTime.MinValue) filtro.InizioCorsoDa = calInizioCorsoDa.SelectedDate;
+
+        if (calInizioCorsoA.SelectedDate != DateTime.MinValue) filtro.InizioCorsoA = calInizioCorsoA.SelectedDate;
+
+        if (calFineCorsoDa.SelectedDate != DateTime.MinValue) filtro.FineCorsoDa = calFineCorsoDa.SelectedDate;
+
+        if (calFineCorsoA.SelectedDate != DateTime.MinValue) filtro.FineCorsoA = calFineCorsoA.SelectedDate;
+
+        ICollection<CorsoEntity> corsiList = repository.Find(filtro);
+        List<CorsoDto> corsiDto = corsiList.Select(c => CorsoMapper.Map(c)).ToList();
+                                                                                                //Aqui, é declarada uma nova lista chamada corsiDto, do tipo List<CorsoDto>.
+                                                                                                //Essa lista vai armazenar objetos do tipo CorsoDto, que é um Data Transfer Object.
+                                                                                                //O CorsoDto é usado para transportar dados entre a camada de acesso a dados e a
+                                                                                                //camada de apresentação, geralmente contendo apenas as informações necessárias para
+                                                                                                //exibir na interface do usuário. O corsoList.Select(r => CorsoMapper.Map(r)) é
+                                                                                                //um método onde o Select é uma operação de LINQ que projeta cada elemento da lista
+                                                                                                //corsoList em um novo formato, o r => CorsoMapper.Map(r) é a parte da expressão
+                                                                                                //que recebe cada CorsoEntity da corsoList(representado por r) e chama o método
+                                                                                                //Map da classe CorsoMapper. O método Map converte um objeto CorsoEntity em um
+                                                                                                //objeto CorsoDto, extraindo as propriedades necessárias e ignorando quaisquer que
+                                                                                                //não são necessárias para a visualização. O ToList() materializa, quando é chamado
+                                                                                                //novamente executa a projeção da consulta e cria uma lista de CorsoDto a partir
+                                                                                                //dos resultados do Select.
+
+          grdRisultati.AutoGenerateColumns = true;                              //O DataGridView automaticamente cria colunas e popula as linhas com os dados
+                                                                                                   //fornecidos na coleção. Isso torna o processo de exibição de dados muito eficiente e
+                                                                                                   //reduz a quantidade de código necessário para configurar o controle. O studentiDto é
+                                                                                                   //uma lista ou coleção de objetos do tipo StudenteDto. O StudenteDto é um Data Transfer
+                                                                                                   //Object(DTO) que representa os dados do estudante de forma simplificada e otimizada
+                                                                                                   //para a transferência, frequentemente utilizado para evitar o acoplamento direto com
+                                                                                                   //a camada de entidade(StudenteEntity). Esta coleção contém dados que foram filtrados
+                                                                                                   //e mapeados a partir da consulta ao banco de dados, conforme a lógica definida
+                                                                                                   //anteriormente.
+
+        grdRisultati.DataSource = corsiDto;                                   //A DataSource é uma propriedade do DataGridView que define a origem dos dados que o
+                                                                                   //controle deve exibir. Ao definir esta propriedade, você informa ao DataGridView de
+
+        grdRisultati.DataBind();
+    }
+
+}

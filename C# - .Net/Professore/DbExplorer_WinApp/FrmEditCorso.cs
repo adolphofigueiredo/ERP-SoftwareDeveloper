b697601d7f4b2f5fa025eeb674e6a9e6b9037c2e
Adolpho Figueiredo <adolphoneto@gmail.com>
2024-10-02 15:22:36 +0200

    20241002_UPDATE

---
+++ C# - .Net/Professore/DbExplorer_WinApp/FrmEditCorso.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DbExplorer_WinApp
+{
+    public partial class FrmEditCorso : Form
+    {
+        public FrmEditCorso()
+        {
+            InitializeComponent();
+        }
+    }
+}

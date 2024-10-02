db2879763ca3c6f1f527bd6fe40aae06ec72c92d
Adolpho Figueiredo <adolphoneto@gmail.com>
2024-10-02 15:26:04 +0200

    20241002_UPDATE

---
+++ C# - .Net/Professore/BusinessLayer/CheckConnectionUtility.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer
+{
+    public static class CheckConnectionUtility
+    {
+        public static bool CheckConnectionString(string connectionString)
+        {
+            if (string.IsNullOrEmpty(connectionString))
+                throw new ArgumentException("Connection strign non valida");
+
+            using (SqlConnection sqlConnection = new SqlConnection(connectionString))
+            {
+                try
+                {
+                    sqlConnection.Open();
+                    sqlConnection.Close();
+                    return true;
+                }
+                catch (Exception)
+                {
+                    return false;
+                }
+            }
+        }
+    }
+}

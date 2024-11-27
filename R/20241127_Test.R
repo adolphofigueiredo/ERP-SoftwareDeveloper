9dd92506ab4a086ba6aa45c285cce7ae1cd273a3
Adolpho Figueiredo <adolphoneto@gmail.com>
2024-11-28 00:47:16 +0100

    20241127_Studying

---
+++ R/20241127_Test.R
@@ -0,0 +1,23 @@
+a <- 10
+b <- 5 
+1 -> c
+
+a
+b
+
+a+b -> d
+
+d
+c
+
+nome  <- c("Adolpho","Leila","Raul","Antonella")
+
+nome
+
+idade <- c(42,38,8,6)
+
+data.frame(nome,idade)
+
+sexo<-c("M","F","M","F")
+
+data.frame(nome,idade,sexo)

4a13be302f25895a3c9b5b0c680e12b6b51b253f
adolphofigueiredo <adolphoneto@gmail.com>
2025-01-23 13:50:03 +0100

    Delete Python/Italiano/Data_Horizon directory

--- Python/Italiano/Data_Horizon/etl_data_erp/app/config.py
+++
@@ -1,8 +0,0 @@
-import os
-
-class Config:
-    SECRET_KEY = os.getenv("SECRET_KEY", "your_secret_key")
-    SHAREPOINT_URL = "https://itsictpiemonte.sharepoint.com/sites/datahorizon"
-    RELATIVE_FOLDER_URL = "/sites/datahorizon/Documenti condivisi/BI_Update_CSV"
-
-config = Config()
